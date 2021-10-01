6e354d2cf9b09701546a5de16d4fce441eeb722c
yash2120s <80688710+yash2120s@users.noreply.github.com>
2021-10-01 23:16:53 +0530

    Add files via upload

---
+++ Navigation/DrawerNavigator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Profile from "../screens/Profile";
+import StackNavigator from "./StackNavigator"
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={StackNavigator} />
+            <Drawer.Screen name="Profile" component={Profile} />
+        </Drawer.Navigator>
+    );
+};
+
+export default DrawerNavigator;
---
+++ config.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+export const firebaseConfig = {
+  apiKey: "AIzaSyAba9EMQNvhWkP9Y3a9dqZQvVI4QwYlw6c",
+  authDomain: "story-telling-22747.firebaseapp.com",
+  projectId: "story-telling-22747",
+  storageBucket: "story-telling-22747.appspot.com",
+  messagingSenderId: "962042306731",
+  appId: "1:962042306731:web:9acfeeebb5280c6a07d7ee"
+};
+
